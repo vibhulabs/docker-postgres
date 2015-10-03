8b20603a590aefc018b27799b9d8a23cebcc2ed7
mayank-io <mayank.kumar@gmail.com>
2015-10-03 14:36:22 +0530

    Completed removed project specific reference. Image is ready to be pushed to docker hub!

--- docker-entrypoint-initdb.d/00-create-extension.sh
+++ docker-entrypoint-initdb.d/00-create-extension.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-gosu postgres psql -p 5432 -U postgres -c "CREATE USER gdb WITH PASSWORD 'MySecretPassw0rd'";
-gosu postgres psql -p 5432 -U postgres -c "CREATE DATABASE garuda OWNER gdb";
+#gosu postgres psql -p 5432 -U postgres -c "CREATE USER gdb WITH PASSWORD 'MySecretPassw0rd'";
+gosu postgres psql -p 5432 -U postgres -c "CREATE USER gdb WITH PASSWORD '$POSTGRES_PASSWORD'";
+gosu postgres psql -p 5432 -U postgres -c "CREATE DATABASE $POSTGRES_DATABASE OWNER gdb";
 
 # This command needs the database to be specified. If this command is run with db name
 # then the extension is created in the default db, postgres, and is not accessible in
 # our db. 
-gosu postgres psql -p 5432 -d garuda -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
+gosu postgres psql -p 5432 -d $POSTGRES_DATABASE -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
