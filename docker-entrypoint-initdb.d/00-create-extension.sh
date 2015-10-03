2c8211aab068c9ce97d5730ef8e891f9dde007f0
Mayank Kumar <mayank.kumar@gmail.com>
2015-10-03 08:14:19 +0000

    Fixed an error. Create extension was running before DB creation, hence it was failing.

--- docker-entrypoint-initdb.d/00-create-extension.sh
+++ docker-entrypoint-initdb.d/00-create-extension.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+gosu postgres psql -p 5432 -U postgres -c "CREATE USER gdb WITH PASSWORD 'MySecretPassw0rd'";
+gosu postgres psql -p 5432 -U postgres -c "CREATE DATABASE garuda OWNER gdb";
+
 # This command needs the database to be specified. If this command is run with db name
 # then the extension is created in the default db, postgres, and is not accessible in
 # our db. 
-gosu postgres psql -p 5432 -d garuda -c "create extension if not exists \"uuid-ossp\";"
+gosu postgres psql -p 5432 -d garuda -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
